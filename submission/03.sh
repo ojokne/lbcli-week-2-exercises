f17afe7b101f6c5548d851e72cc68b6546b3de79
Ojok Emmanuel Nsubuga <ojokne@gmail.com>
2025-03-24 12:17:11 +0300

    Answer question three

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,7 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+segwitaddress=$(bitcoin-cli -regtest getnewaddress "p2sh-segwit")
+
+bitcoin-cli -regtest generatetoaddress 101 $segwitaddress | jq -r ".address"
